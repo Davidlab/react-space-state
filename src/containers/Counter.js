167c62f720fcf3cbdfb3c4878bfa3cffffc11e6a
David Labbe <dlabbe72@gmail.com>
2021-10-13 18:50:50 -0400

    Update Counter.js

--- src/containers/Counter.js
+++ src/containers/Counter.js
@@ -1,14 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 
-import { useStore } from "../store/counter-store";
+import { store } from "../store/counter-store";
 import "./Counter.css";
 const Counter = (props) => {
-  const [state, dispatch] = useStore();
+  const getState = store.getState();
+  const dispatch = store.useDispatch(false);
 
   return (
     <div className="counter">
       <p>Only there to proof, that you can have multiple state slices.</p>
-      <p>Counter: {state.counter}</p>
+      <p>Counter: {getState.counter}</p>
       <button onClick={() => dispatch("ADD", 1)}>Add 1</button>
       <button onClick={() => dispatch("ADD", 5)}>Add 5</button>
       <button onClick={() => dispatch("SUB", 1)}>Subtract 1</button>
