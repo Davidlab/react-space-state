a90c17753f4d1c54cb752bc9fbe22c73221e305e
David Labbe <dlabbe72@gmail.com>
2021-10-13 18:51:42 -0400

    Update CounterSubscribe.js

--- src/containers/CounterSubscribe.js
+++ src/containers/CounterSubscribe.js
@@ -1,23 +1,23 @@
 import React from "react";
 
-import { useStore } from "../store/counter-store";
+import { store } from "../store/counter-store";
 import "./Counter.css";
 
 //Example using subscribe to store
 //class.useStore(boolean, function(newState, oldState) =>{return boolean. Return true to run, false not to run on dispatch call}
 const CountSubscribe = (props) => {
-  const state = useStore(true, (newState, oldState) => {
+  const state = store.getState((newState, oldState) => {
     //Will only run this state update with counter hits five
     // return newState.counter === 5;
     if (newState.counter % 2 === 0) {
-      return true;
       console.log("Even");
+      return true;
     } else {
-      return false;
       console.log("Odd");
+      return false;
     }
     //Note always return false if you don't want to subscribe
-  })[0];
+  });
 
   return (
     <div className="counter">
